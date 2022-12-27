de3ab4f972a77b10b679e0d89fbcf3e77dbe57fc
Foxtrot <97063668+Foxtrot531@users.noreply.github.com>
2022-12-27 22:23:38 +0100

    chore(ox_inventory):edit shops

--- data/shops.lua
+++ data/shops.lua
@@ -10,7 +10,7 @@
 
 return {
 	General = {
-		name = 'Shop',
+		name = '24/7',
 		blip = {
 			id = 59, colour = 69, scale = 0.8
 		}, inventory = {
@@ -82,40 +82,119 @@ return {
 		}
 	},
 
-	Ammunation = {
-		name = 'Ammunation',
+
+	DigitalDen = {
+		name = 'DigitalDen',
 		blip = {
-			id = 110, colour = 69, scale = 0.8
+			id = 817, colour = 83, scale = 0.8
 		}, inventory = {
-			{ name = 'ammo-9', price = 5, },
-			{ name = 'WEAPON_KNIFE', price = 200 },
-			{ name = 'WEAPON_BAT', price = 100 },
-			{ name = 'WEAPON_PISTOL', price = 1000, metadata = { registered = true }, license = 'weapon' }
+			{ name = 'phone', price = 1459 }
 		}, locations = {
-			vec3(-662.180, -934.961, 21.829),
-			vec3(810.25, -2157.60, 29.62),
-			vec3(1693.44, 3760.16, 34.71),
-			vec3(-330.24, 6083.88, 31.45),
-			vec3(252.63, -50.00, 69.94),
-			vec3(22.56, -1109.89, 29.80),
-			vec3(2567.69, 294.38, 108.73),
-			vec3(-1117.58, 2698.61, 18.55),
-			vec3(842.44, -1033.42, 28.19)
+			vec3(-1232.0, -1440.5, 4.0)
 		}, targets = {
-			{ loc = vec3(-660.92, -934.10, 21.94), length = 0.6, width = 0.5, heading = 180.0, minZ = 21.8, maxZ = 22.2, distance = 2.0 },
-			{ loc = vec3(808.86, -2158.50, 29.73), length = 0.6, width = 0.5, heading = 360.0, minZ = 29.6, maxZ = 30.0, distance = 2.0 },
-			{ loc = vec3(1693.57, 3761.60, 34.82), length = 0.6, width = 0.5, heading = 227.39, minZ = 34.7, maxZ = 35.1, distance = 2.0 },
-			{ loc = vec3(-330.29, 6085.54, 31.57), length = 0.6, width = 0.5, heading = 225.0, minZ = 31.4, maxZ = 31.8, distance = 2.0 },
-			{ loc = vec3(252.85, -51.62, 70.0), length = 0.6, width = 0.5, heading = 70.0, minZ = 69.9, maxZ = 70.3, distance = 2.0 },
-			{ loc = vec3(23.68, -1106.46, 29.91), length = 0.6, width = 0.5, heading = 160.0, minZ = 29.8, maxZ = 30.2, distance = 2.0 },
-			{ loc = vec3(2566.59, 293.13, 108.85), length = 0.6, width = 0.5, heading = 360.0, minZ = 108.7, maxZ = 109.1, distance = 2.0 },
-			{ loc = vec3(-1117.61, 2700.26, 18.67), length = 0.6, width = 0.5, heading = 221.82, minZ = 18.5, maxZ = 18.9, distance = 2.0 },
-			{ loc = vec3(841.05, -1034.76, 28.31), length = 0.6, width = 0.5, heading = 360.0, minZ = 28.2, maxZ = 28.6, distance = 2.0 }
+			{ loc = vec3(-1232.0, -1440.5, 4.0), length = 1.25, width = 0.75, heading = 0.35, minZ = 55.0, maxZ = 56.8, distance = 3.0 }
 		}
 	},
 
+	Brewery = {
+		name = 'Brewery',
+		blip = {
+			id = 817, colour = 56, scale = 0.8
+		}, inventory = {
+			{ name = 'phone', price = 1459 }
+		}, locations = {
+			vec3(-1225.95, -1439.5, 3.85)
+		}, targets = {
+			{ loc = vec3(-1225.95, -1439.5, 3.85), length = 1.5, width = 1.0, heading = 0.35, minZ = 55.0, maxZ = 56.8, distance = 3.0 }
+		}
+	},
+
+	TruthOrganic = {
+		name = 'Truth Organic',
+		blip = {
+			id = 469, colour = 66, scale = 0.8
+		}, inventory = {
+			{ name = 'phone', price = 1459 }
+		}, locations = {
+			vec3(-1205.75, -1460.1, 4.0)
+		}, targets = {
+			{ loc = vec3(-1205.75, -1460.1, 4.0), length = 0.5, width = 1.75, heading = 0.35, minZ = 55.0, maxZ = 56.8, distance = 3.0 }
+		}
+	},
+
+	LDOrganies = {
+		name = 'LD Organies',
+		blip = {
+			id = 140, colour = 69, scale = 0.8
+		}, inventory = {
+			{ name = 'phone', price = 1459 }
+		}, locations = {
+			vec3(-1210.55, -1464.55, 4.0)
+		}, targets = {
+			{ loc = vec3(-1210.55, -1464.55, 4.0), length = 4.0, width = 0.5, heading = 305.5, minZ = 55.0, maxZ = 56.8, distance = 3.0 }
+		}
+	},
+
+	GoodBeans = {
+		name = 'Good Beans',
+		blip = {
+			id = 106, colour = 61, scale = 0.8
+		}, inventory = {
+			{ name = 'phone', price = 1459 }
+		}, locations = {
+			vec3(-1215.5, -1468.1, 4.0)
+		}, targets = {
+			{ loc = vec3(-1215.5, -1468.1, 4.0), length = 2.5, width = 1.0, heading = 0.35, minZ = 55.0, maxZ = 56.8, distance = 3.0 }
+		}
+	},
+
+	PopsPills = {
+		name = 'Pop\'s Pills',
+		blip = {
+			id = 51, colour = 1, scale = 0.8
+		}, inventory = {
+			{ name = 'phone', price = 1459 }
+		}, locations = {
+			vec3(-1198.9, -1459.7, 3.75)
+		}, targets = {
+			{ loc = vec3(-1198.9, -1459.7, 3.75), length = 0.5, width = 1.5, heading = 0.35, minZ = 55.0, maxZ = 56.8, distance = 3.0 },
+			{ loc = vec3(-1196.55, -1458.1, 4.0), length = 0.7, width = 1.4, heading = 0.35, minZ = 55.0, maxZ = 56.8, distance = 3.0 }
+		}
+	},
+--	Ammunation = {
+	--	name = 'Ammunation',
+	--	blip = {
+	--		id = 110, colour = 69, scale = 0.8
+		--}, inventory = {
+		--	{ name = 'ammo-9', price = 5, },
+		--	{ name = 'WEAPON_KNIFE', price = 200 },
+		--	{ name = 'WEAPON_BAT', price = 100 },
+			--{ name = 'WEAPON_PISTOL', price = 1000, metadata = { registered = true }, license = 'weapon' }
+		--}, locations = {
+		--	vec3(-662.180, -934.961, 21.829),
+		--	vec3(810.25, -2157.60, 29.62),
+		--	vec3(1693.44, 3760.16, 34.71),
+		--	vec3(-330.24, 6083.88, 31.45),
+		--	vec3(252.63, -50.00, 69.94),
+		--	vec3(22.56, -1109.89, 29.80),
+		--	vec3(2567.69, 294.38, 108.73),
+		--	vec3(-1117.58, 2698.61, 18.55),
+		--	vec3(842.44, -1033.42, 28.19)
+		--}, targets = {
+		--	{ loc = vec3(-660.92, -934.10, 21.94), length = 0.6, width = 0.5, heading = 180.0, minZ = 21.8, maxZ = 22.2, distance = 2.0 },
+		--	{ loc = vec3(808.86, -2158.50, 29.73), length = 0.6, width = 0.5, heading = 360.0, minZ = 29.6, maxZ = 30.0, distance = 2.0 },
+		--	{ loc = vec3(1693.57, 3761.60, 34.82), length = 0.6, width = 0.5, heading = 227.39, minZ = 34.7, maxZ = 35.1, distance = 2.0 },
+		--	{ loc = vec3(-330.29, 6085.54, 31.57), length = 0.6, width = 0.5, heading = 225.0, minZ = 31.4, maxZ = 31.8, distance = 2.0 },
+		--	{ loc = vec3(252.85, -51.62, 70.0), length = 0.6, width = 0.5, heading = 70.0, minZ = 69.9, maxZ = 70.3, distance = 2.0 },
+		--	{ loc = vec3(23.68, -1106.46, 29.91), length = 0.6, width = 0.5, heading = 160.0, minZ = 29.8, maxZ = 30.2, distance = 2.0 },
+		--	{ loc = vec3(2566.59, 293.13, 108.85), length = 0.6, width = 0.5, heading = 360.0, minZ = 108.7, maxZ = 109.1, distance = 2.0 },
+		--	{ loc = vec3(-1117.61, 2700.26, 18.67), length = 0.6, width = 0.5, heading = 221.82, minZ = 18.5, maxZ = 18.9, distance = 2.0 },
+		--	{ loc = vec3(841.05, -1034.76, 28.31), length = 0.6, width = 0.5, heading = 360.0, minZ = 28.2, maxZ = 28.6, distance = 2.0 }
+	--	}
+	--},
+
 	PoliceArmoury = {
-		name = 'Police Armoury',
+		name = 'Armurerie',
 		groups = shared.police,
 		blip = {
 			id = 110, colour = 84, scale = 0.8
@@ -135,7 +214,7 @@ return {
 	},
 
 	Medicine = {
-		name = 'Medicine Cabinet',
+		name = 'Pharmacie',
 		groups = {
 			['ambulance'] = 0
 		},
@@ -167,7 +246,7 @@ return {
 	},
 
 	VendingMachineDrinks = {
-		name = 'Vending Machine',
+		name = 'distributeur',
 		inventory = {
 			{ name = 'water', price = 10 },
 			{ name = 'cola', price = 10 },
